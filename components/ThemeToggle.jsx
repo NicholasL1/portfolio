aa1165c7cbab1d6ed8a324c0f7fb351629ee60c6
Nicholas Lachhman <nickla@my.yorku.ca>
2024-07-05 02:30:25 -0400

    Cleaned ThemeToggle code

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -25,19 +25,17 @@ const ThemeToggle = () => {
   }, [darkMode]);
 
   return (
-    <ThemeContext.Provider value={darkMode}>
+    <div
+      className="relative w-16 h-8 flex items-center dark:bg-gray-800 bg-blue-400 cursor-pointer rounded-full p-1"
+      onClick={() => setDarkMode(!darkMode)}
+    >
+      <FaMoon className="text-white" size={18} />
       <div
-        className="relative w-16 h-8 flex items-center dark:bg-gray-800 bg-blue-400 cursor-pointer rounded-full p-1"
-        onClick={() => setDarkMode(!darkMode)}
-      >
-        <FaMoon className="text-white" size={18} />
-        <div
-          className="absolute bg-white dark:bg-medium w-6 h-6 rounded-full shadow-md transform transition-transform duration-300"
-          style={darkMode ? { left: "2px" } : { right: "2px" }}
-        ></div>
-        <BsSunFill className="ml-auto text-yellow-400" size={18} />
-      </div>
-    </ThemeContext.Provider>
+        className="absolute bg-white dark:bg-medium w-6 h-6 rounded-full shadow-md transform transition-transform duration-300"
+        style={darkMode ? { left: "2px" } : { right: "2px" }}
+      ></div>
+      <BsSunFill className="ml-auto text-yellow-400" size={18} />
+    </div>
   );
 };
 
